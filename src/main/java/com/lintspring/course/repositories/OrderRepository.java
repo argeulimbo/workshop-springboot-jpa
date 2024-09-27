afd1d8d873f6b0d418c7a821fe23c0b3a960a50f
argeulimbo <argeusousaptbr@gmail.com>
2024-09-27 14:39:15 -0300

    Order, Instant, ISO 8601 - p.02

---
+++ src/main/java/com/lintspring/course/repositories/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.lintspring.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lintspring.course.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+	
+	
+}
