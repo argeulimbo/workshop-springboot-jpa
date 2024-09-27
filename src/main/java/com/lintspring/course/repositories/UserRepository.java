121356c583bc914223f5d2584b316979e602ca1c
argeulimbo <argeusousaptbr@gmail.com>
2024-09-27 10:19:03 -0300

    Service layer, component registration

--- src/main/java/com/lintspring/course/repositories/UserRepository.java
+++ src/main/java/com/lintspring/course/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.lintspring.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lintspring.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	
